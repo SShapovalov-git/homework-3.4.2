d0829e657e28e48aa25bb986a0cabefc2b973258
sergei Shapovalov <senishapovalov@gmail.com>
2020-05-13 17:42:22 +0300

    CorrectorsII

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        long height = 185;
-        long weight = 90;
-        long BMI = service.calculate(height, weight);
-        System.out.println(BMI);
+        int height = 185;
+        int weight = 90;
+        int BodyMassIndex = (int) service.calculate(height, weight);
+        System.out.println(BodyMassIndex);
     }
 }
